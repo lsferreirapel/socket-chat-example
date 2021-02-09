be9886dbb6389586be2e9297d776aaaddf3b6d7f
Lucas Ferreira <lsferreirapel@gmail.com>
2021-02-09 19:01:46 -0300

    :art: Implement default setup to real time chat

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
-
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => console.log('🔥 Server linstening on port:3000'));
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  // socket.on('disconnect', () => {
+  //   console.log('a user disconnected');
+  // })
+  // socket.on('chat message', msg => {
+  //   console.log(`message: ${msg}`)
+  // });
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => console.log('🔥 Server linstening on port:3000'));
