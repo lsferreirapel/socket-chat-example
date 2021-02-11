a147e0cc9e581c62fd21c13e3c051ab1c58cc7c6
Lucas Ferreira <lsferreirapel@gmail.com>
2021-02-10 23:39:21 -0300

    :art: Improve code structure and connection config
    Implemented fuction to send user information to client side on connection,
    there were minor changes in the code too.

--- src/server.js
+++ src/server.js
@@ -5,61 +5,67 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
 
+const auth = require('./services/auth');
+
 const app = express();
 const server = http.createServer(app);
 const sockets = socketio(server);
 
-
-const jwt = require('jsonwebtoken');
-
+// Express configs
 app.use(cors());
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(cookieParser());
 
+// If token is valid send user to chat else send user to login page 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/login.html');
 });
 
 app.post('/login', (req, res) => { 
+
+  // Get info from body
   const { username, color } = req.body;
 
-  const token = jwt.sign(
-    { username, color},
-    "@QEGTUI",
-    { expiresIn: '1h' }
-  );
-  console.log(token);
+  // Create a new token
+  const token = auth.createNewToken(username, color);
   
+  // Save token on cookies
   res.cookie('token', token);
+
+  // Send user to chat page
   res.status(200).redirect('/chat');
 });
 
+
 app.get('/chat', (req, res) => {
+  // Get token from cookies
   const token = req.cookies.token;
-  console.log('chat');
-  console.log(token);
-  if(token) {
-    res.sendFile(__dirname + '/views/chat.html')
+  
+  // Validate token, if is valid send user to chat page
+  //  else send user to login page
+  if(auth.tokenIsValid(token)) {
+    res.sendFile(__dirname + '/views/chat.html');
   } else {
     res.redirect('/');
   }
+
 })
 
 sockets.on('connection', (socket) => {
-  
-  // Send alert to server side when user connect and disconnect
-  socket.broadcast.emit('user connection', 'a user connected');
-  socket.on('disconnect', () => {
-    sockets.emit('user connection', 'a user disconnected');
-  });
+  // Get token from socket handshake, "set on client-side"
+  let token = socket.handshake.auth.token;
 
-  // socket.on('chat message', msg => {
-  //   console.log(`message: ${msg}`)
-  // });
+  // Get user info form token
+  let user = auth.getUserFromToken(token);
+
+
+  // Send alert to server side when user connect and disconnect
+  socket.broadcast.emit('user connection', `${user.username} connected`);
+  socket.on('disconnect', () => {
+    sockets.emit('user connection', `${user.username} disconnected`);
+  });
 
   // Send the user message to server side
   socket.on('chat message', msg => {
@@ -67,4 +73,5 @@ sockets.on('connection', (socket) => {
   });
 });
 
+// Start server
 server.listen(3000, () => console.log('🔥 Server linstening on port:3000'));
